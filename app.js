29856839c0300e503ecbe37d8cdb2f230cd9b248
Naim <88440061+FollowNaim@users.noreply.github.com>
2024-05-06 01:59:20 +0600

    Update app.js

--- app.js
+++ app.js
@@ -170,7 +170,10 @@ function nav(path) {
 	var search_text = model.is_search_page ? (model.q || '') : '';
 	var search_bar = `
 </ul>
-<div class="btn btn-warning"><a href="https://banglasubtitle.pages.dev">Back To Search</a></div>
+<form class="d-flex" method="get" action="/${cur}:search">
+<input class="form-control me-2" name="q" type="search" placeholder="Search" aria-label="Search" value="${search_text}" required>
+<button class="btn ${UI.search_button_class}" onclick="if($('#search_bar_form>input').val()) $('#search_bar_form').submit();" type="submit">Search</button>
+</form>
 </div>
 </div>
 </nav>
